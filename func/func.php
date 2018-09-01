a99d85a9ff52eb9d6db66c8a406324b68ab89db7
tasuku <tasuku1215mhg@gmail.com>
2018-09-01 16:23:15 +0900

    ファイル作成

---
+++ func/func.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* h
+* 引数$strに入っている文字列からHTMLタグを無効化する
+* @param $str文字列HTMLタグが入っている可能性がある
+* @return HTMLを無効化した文字列を返す
+*/
+function h($str){
+  return htmlspecialchars($str,ENT_QUOTES);
+}
+
+/**
+* session_delete
+* すべてのsessionを削除する
+*/
+function session_delete(){
+  $_SESSION = array();
+  if(isset($_COOKIE[SESSION_name()])){
+    setcookie(session_name(),'',time()-42000);
+  }
+  session_destroy();
+}
+function tasi($val,$val2){
+  return $val+$val2;
+}
+?>
---
+++ tpl/index.php
@@ -0,0 +1,6 @@
+<?php
+// ヘッダーHTML呼び出し
+  require_once './tpl/header.html';
+  // フッターHTML呼び出し
+  require_once './tpl/footer.html';
+?>
