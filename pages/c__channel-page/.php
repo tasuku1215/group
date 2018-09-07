c52fbd1e695bbdcc4a0ec28b9e32521291497624
yosa34 <osaru.san.0234@gmail.com>
2018-09-07 20:33:51 +0900

    チャンネルページ　作成途中

--- pages/c__channel-page/.php
+++ pages/c__channel-page/.php
@@ -7,5 +7,99 @@
 //====================================================
 ?>
 <div class="c__main">
-
+    <!-- head -->
+    <div>
+        <div><img src="" alt=""></div>
+    </div>
+    <!-- 情報 -->
+    <div>
+        <h2>ユーザー名</h2>
+        <p>動画数：999</p>
+    </div>
+    <div>
+        <h3>投稿動画</h3>
+        <select name="kibun">
+            <option value="1">再生回数の多い</option>
+            <option value="2">再生回数の少ない<option>
+            <option value="3">投稿時間の古い</option>
+            <option value="4">投稿時間の新しい</option>
+        </select>
+    </div>
+    <div class="p__video_list">
+        <ul>
+            <li>
+                <img src="video_icon/firstframe.jpg" alt="動画アイコン">
+                <div>
+                    <h3>動画名</h3>
+                    <p>お気に入り数/999</p>
+                    <p>当が再生回数</p>
+                    <p>投稿時間</p>
+                </div>
+            </li>
+            <li>
+                <img src="video_icon/firstframe.jpg" alt="動画アイコン">
+                <div>
+                    <h3>動画名</h3>
+                    <img src="" alt="投稿者アイコン"><p>投稿者名</p>
+                    <p>お気に入り数/999</p>
+                </div>
+            </li>
+            <li>
+                <img src="video_icon/firstframe.jpg" alt="動画アイコン">
+                <div>
+                    <h3>動画名</h3>
+                    <img src="" alt="投稿者アイコン"><p>投稿者名</p>
+                    <p>お気に入り数/999</p>
+                </div>
+            </li>
+            <li>
+                <img src="video_icon/firstframe.jpg" alt="動画アイコン">
+                <div>
+                    <h3>動画名</h3>
+                    <img src="" alt="投稿者アイコン"><p>投稿者名</p>
+                    <p>お気に入り数/999</p>
+                </div>
+            </li>
+            <li>
+                <img src="video_icon/firstframe.jpg" alt="動画アイコン">
+                <div>
+                    <h3>動画名</h3>
+                    <img src="" alt="投稿者アイコン"><p>投稿者名</p>
+                    <p>お気に入り数/999</p>
+                </div>
+            </li>
+            <li>
+                <img src="video_icon/firstframe.jpg" alt="動画アイコン">
+                <div>
+                    <h3>動画名</h3>
+                    <img src="" alt="投稿者アイコン"><p>投稿者名</p>
+                    <p>お気に入り数/999</p>
+                </div>
+            </li>
+            <li>
+                <img src="video_icon/firstframe.jpg" alt="動画アイコン">
+                <div>
+                    <h3>動画名</h3>
+                    <img src="" alt="投稿者アイコン"><p>投稿者名</p>
+                    <p>お気に入り数/999</p>
+                </div>
+            </li>
+            <li>
+                <img src="video_icon/firstframe.jpg" alt="動画アイコン">
+                <div>
+                    <h3>動画名</h3>
+                    <img src="" alt="投稿者アイコン"><p>投稿者名</p>
+                    <p>お気に入り数/999</p>
+                </div>
+            </li>
+            <li>
+                <img src="video_icon/firstframe.jpg" alt="動画アイコン">
+                <div>
+                    <h3>動画名</h3>
+                    <img src="" alt="投稿者アイコン"><p>投稿者名</p>
+                    <p>お気に入り数/999</p>
+                </div>
+            </li>
+        </ul>
+    </div>
 </div>
