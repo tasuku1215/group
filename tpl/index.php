b603c727c9f1bd01570143a14c5475ff52ff0e15
tasuku <tasuku1215mhg@gmail.com>
2018-09-08 11:50:53 +0900

    最初に表示されるページに全ページへのリンク作成

--- tpl/index.php
+++ tpl/index.php
@@ -1,6 +1,22 @@
 <?php
 // ヘッダーHTML呼び出し
   require_once './tpl/header.html';
+  ?>
+  <a href="./pages/c__channel-page/index.php">c__channel-page</a>
+  <br>
+  <a href="./pages/c__login/index.php">c__login</a>
+  <br>
+  <a href="./pages/ｃ__menber-registration/index.php">ｃ__menber-registration</a>
+  <br>
+  <a href="./pages/c__my_page/index.php">c__my_page</a>
+  <br>
+  <a href="./pages/c__top-page/index.php">c__top-page</a>
+  <br>
+  <a href="./pages/c__video-registration/index.php">c__video-registration</a>
+  <br>
+  <a href="./pages/c__video-show/index.php">c__video-show</a>
+  <br>
+  <?php
   // フッターHTML呼び出し
   require_once './tpl/footer.html';
 ?>
