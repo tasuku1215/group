aaa5b507c4f8c3411ed8fac1182f77e9be9398ff
WIN10OHS70249\ohs70249 <hozuka.zero3zero8@gmail.com>
2018-09-27 10:20:11 +0900

    投稿画面　ｃｓｓ　

--- pages/c__video-registration/tpl/index.php
+++ pages/c__video-registration/tpl/index.php
@@ -11,67 +11,72 @@ require_once './../../tpl/left_menu.html';
 
 ?>
 <div class="c__main">
-   <!---ディレクトリ表示 -->
-   <div id=""></div>
 <div id="upload_portbox_box">
-    <form action="">
-      <h2>動画投稿</h2>
+  <!---ディレクトリ表示 -->
+  <div id="directory_box">
+   <a href="#" class="square_btn">TOPページ</a>
+   <a href="#" class="square_btn">チャンネル登録</a>
+   </div>
+  <form action="#">
+     <div class="heding"> <h2>動画投稿</h2></div>
       <table>
         <tr>
-          <th>投稿動画</th>
+          <th class ="aline-middle" width="200" style="font-size : 20px;"><h3>投稿動画</h3></th>
         </tr>
         <tr>
-          <td><input type="file" name="upload_file" alt="動画アップロード"></td>
+          <td><input class ="sample"type="file" name="upload_file" alt="動画アップロード"></td>
         </tr>
         <tr>
-          <th>金額設定</th>
+          <th class ="aline-middle" style="font-size : 20px;"><h3>金額設定</h3></th>
           <td>
-            <p>
-              <label for="language">金額設定してください</label>
-              <select id="language" name="language">
-                <option value="en">0</option>
-                <option value="de" disabled>500</option>
-                <option value="fr">1000</option>
-                <option value="ja" selected>5000</option>
-                <option value="zh">10000</option>
+            <div class="form-select">
+              <select id="select-money" name="select-money">
+                <option value=""></option>
+                  <option value="money1">0</option>
+                  <option value="money2" >500</option>
+                  <option value="money3">1000</option>
+                  <option value="money4" >5000</option>
+                  <option value="money5">10000</option>
               </select>
-            </p>
+              </div>
           </td>
         </tr>
         <tr>
-          <th>公開範囲</th>
+          <th class ="aline-middle"><h3 style="font-size : 20px;">公開範囲</h3></th>
           <td>
-            <p>
-              <input type="radio" name="comon">誰でも
-              <input type="radio" name="menber">限定公開
-              <input type="radio" name="private">非公開
-            </p>
+          <div class="form-select">
+              <select id="select-release" name="select-release">
+                <option value=""></option>
+                  <option value="release1">自分だけ</option>
+                  <option value="release2" >一般</option>
+                  <option value="release3">会員</option>
+              </select>
+   </div>  
           </td>
         </tr>
         <tr>
-          <th>動画タイトル</th>
+          <th class ="aline-middle"><h3 style="font-size : 20px;">動画タイトル</h3></th>
           <td>
             <input type="text" name="title">
           </td>
         </tr>
         <tr>
-          <th>
-            動画説明
+          <th class ="aline-middle">
+          <h3 style="font-size : 20px;">動画説明</h3>
           </th>
           <td>
-            <input type="text" name="explanatory" alt="動画説明文">
+            <textarea name="explanatory" alt="動画説明文"rows=10 cols=50>
+            </textarea>
           </td>
         </tr>
       </table>
       <div id="upload_buttun">
         <button type="button" name="sned" value="" alt="OKボタン">
-          <font size="2">ここを</font>
-          <font size="5" color="#333399">OK</font>
+          <p size="5" >OK</p>
         </button>
       </div>
     </div>
   </form>
-
 </div>
 <?php
 require_once "./../../tpl/footer.html";
