0f19a3460526db6ac7a24ae146f125ceb9bece73
WIN10OHS70249\ohs70249 <hozuka.zero3zero8@gmail.com>
2018-10-05 12:28:53 +0900

    チャンネル開設　確認ページ　完了ページ　プロフィール変更ページ　確認

---
+++ pages/c__channel_opening/tpl/index.php
@@ -0,0 +1,38 @@
+<?php
+//====================================================
+//チャンネルページ
+//
+//以下エラー文
+//
+//====================================================
+require_once "./../../tpl/header.html";
+  require_once './../../tpl/main_up_menu.html';
+  require_once './../../tpl/left_menu.html';
+?>
+<div class="c__main">
+  <div class="channel_op">
+
+    <div class="directory_box">
+                <a href="#" class="square_btn">TOPページ</a>
+                <div class="header_font">
+                    <h2>チャンネル登録ページ</h2>
+                </div>
+     </div>
+     <div class="channel_detail">
+       <div class="box_center">
+         <h3 style="font-size : 20px;">チャンネル名</h3>
+         <p><input type="text"></p>
+         <h3 style="font-size : 20px;">チャンネル説明</h3>
+         <p><textarea name="" id="" cols="30" rows="20"></textarea></p>
+        </div>
+        <div class="upload_buttun">
+          <button type="button" name="sned" value="" alt="OKボタン">
+            <p size="5" >OK</p>
+          </button>
+        </div>
+      </div>
+    </div>
+    </div>
+    <?php
+require_once "./../../tpl/footer.html";
+?>
---
+++ pages/c__profile-change/tpl/index.php
@@ -0,0 +1,62 @@
+<?php
+//====================================================
+//プロフィール変更ページ
+//
+//以下エラー文
+//
+//====================================================
+require_once "./../../tpl/header.html";
+  require_once './../../tpl/main_up_menu.html';
+  require_once './../../tpl/left_menu.html';
+?>
+<div class="c__main">
+  <!-- profile-change　プロフィール変更 -->
+  <div class="profile_change_box">
+    <div class="directory_box">
+      <a href="#" class="square_btn">TOPページ</a>
+      <a href="#" class="square_btn">マイぺージ</a>
+      <a href="#" class="square_btn">変更ぺージ</a>
+      <div class="header_font">
+                <h2>プロフィール変更</h2>
+            </div>
+    </div>
+    <div class="profile_more_detail">
+      <div class="detail_top">
+        <div class="img_box">
+          <div class="img_b">
+            <img src="" alt="プロフィール画像">
+          </div>
+          <div class="center">
+          <button type="button" name="select"" value="" alt=" 変更">
+            <font size="2" color="#333399">選択 </font>
+          </button>
+          </div>
+        </div>
+        <div class="introduction_box ">
+        <h3>名前</h3>
+        <input type="text">
+        <h3>コメント</h3>
+        <textarea name="" id="" cols="95"" rows="10">
+        </textarea>
+      </div>
+      <div class="damy"> 
+      </div>
+      <div class="detail_bottom">
+        <p>プライバシー</p>
+          <p>マイリストをすべて非公開</p>
+          <p>お気に入りをすべて非公開</p>
+        </div>
+        <div class="center">
+        <button type="button" name="change" value="" alt="変更">
+        <font size="2" color="#333399">変更 </font>
+    </button>
+        </div>
+      </div>
+    </div>
+   
+  </div>
+
+</div>
+<?php
+require_once "./../../tpl/footer.html";
+?>
