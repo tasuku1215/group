6d3d093aad44b8da0cf053db2ef4f6896e0ecdbe
TsujiRyota <xx.ryota.xx.brh0422@i.softbank.jp>
2018-10-09 02:55:10 +0900

    こみいいとお

--- pages/c__video-show/index.php
+++ pages/c__video-show/index.php
@@ -3,6 +3,26 @@
   require_once './../../config.php';
   // ファンクション呼び出し
   require_once './../../func/func.php';
+  
+  $video_id = $_GET['video_id'];
+
+  $link = mysqli_connect(HOST , DB_USER , DB_PASS , DB_NAME);
+  mysqli_set_charset($link , 'utf8');
+  
+
+  $sql = "SELECT * FROM video WHERE video_id = ".$video_id;
+  var_dump($sql);
+
+
+  $result = mysqli_query($link , $sql);
+  while($row = mysqli_fetch_assoc($result)){
+    $video_list[] = $row; 
+  }
+
+  var_dump($video_list);
+  mysqli_close($link);
+
+
   // テンプレート呼び出し
   require_once './tpl/index.php';
 ?>
